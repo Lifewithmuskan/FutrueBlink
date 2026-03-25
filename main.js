6d0e5bec447e17ac908f9724a7424d76e088c947
Lifewiithmuskan <muskan112002muskan@gmail.com>
2026-03-25 20:54:15 +0530

    waiting

--- main.js
+++ main.js
@@ -54,7 +54,7 @@ app.post("/api/ask-ai",async(req,res)=>{
         },
         {
             headers:{
-                 " Authorization": `Bearer ${process.env.OPENROUTER_API_KEY}`,
+                 "Authorization": `Bearer ${process.env.OPENROUTER_API_KEY}`,
                     "Content-Type": "application/json",
                     "HTTP-Referer": "http://localhost:3000",
                     "X-Title": "My App"
